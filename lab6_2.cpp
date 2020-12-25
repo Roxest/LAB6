75e5e7ded11252a8aa0dd2f9853297b2ceefc02b
lab302-8 <lab302-8@labeng.local>
2020-12-25 16:35:16 +0700

    lab6_2 completed

--- lab6_2.cpp
+++ lab6_2.cpp
@@ -16,3 +16,41 @@ Fahsai: Next Monday....that is OK!!! I'm looking forward to watching Star Wars V
 Luke Skywalker: May the Force be with you krub
 Fahsai: 555+ see you Next Monday. Bye Bye \(^ ^)/
 */
+#include <iostream>
+#include <string>
+using namespace std;
+
+void p1chat(string name){
+    cout << name << ": ";
+}
+
+int main(){
+    string username,movie,dapp,ans;
+    int id;
+    cout << "Fahsai: Sawadee ka...Can you tell me your name?";
+    cout << "\n?????: ";
+    getline(cin,username);
+    cout << "Fahsai: Wow!!! "<< username <<" is a really cool name.\n";
+    cout << "Fahsai: I think you are an Engineering student. What is your student ID?\n";
+    p1chat(username);
+    cin >> id;
+    int gear = (id/10000000)-12;
+    cin.ignore();
+    cout << "Fahsai: I think you may be GEAR "<< gear  <<". I have a free movie ticket for you.\n";
+
+    cout << "Fahsai: Let's go to the cinema together!!!\n";
+    cout << "Fahsai: What movie do you want to watch?\n";
+    p1chat(username);
+    getline(cin,movie);
+
+    cout << "Fahsai: So....which day are you free to go with me?\n";
+    p1chat(username);
+    getline(cin,dapp);
+
+    cout << "Fahsai:"<< dapp <<"....that is OK!!! I'm looking forward to watching "<< movie <<" with you.\n";
+    p1chat(username);
+    getline(cin,ans);
+
+    cout << "Fahsai: 555+ see you "<< dapp <<". Bye Bye \(^ ^)/";
+    return 0;
+}
